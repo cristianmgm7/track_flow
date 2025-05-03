065d45328b1a41e6282d793eb1ab78119e262763
Cristian Murillo <cristianmgm7@gmail.com>
2025-05-03 22:39:07 +0200

    Add Firebase integration and configuration
    - Added firebase.json for Firebase project configuration.
    - Included GoogleService-Info.plist for iOS and GoogleService-Info.plist for macOS.
    - Updated pubspec.yaml and pubspec.lock to include firebase_auth and firebase_core dependencies.
    - Configured Firebase initialization in main.dart.
    - Created firebase_options.dart for platform-specific Firebase options.
    - Updated Android and iOS project files to support Firebase services.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:trackflow/screens/auth.dart';
 import 'package:trackflow/theme/theme.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
