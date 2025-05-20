0f0d3e45c167b12086d9a6b5731e8baa3595020d
Thomas Viana <thomasviana16@gmail.com>
2025-05-20 12:07:22 -0500

    refactor: restructure main application entry point and move MyApp to a separate file for better organization and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:trackflow/core/constants/theme.dart';
-import 'package:trackflow/core/router/app_router.dart';
-import 'package:trackflow/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:trackflow/features/auth/presentation/bloc/auth_event.dart';
-import 'package:trackflow/features/onboarding/presentation/bloc/onboarding_bloc.dart';
-import 'package:trackflow/features/onboarding/presentation/bloc/onboarding_event.dart';
-import 'package:trackflow/features/onboarding/data/repositories/shared_prefs_onboarding_repository.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:trackflow/features/projects/presentation/blocs/projects_bloc.dart';
+import 'package:trackflow/core/app/my_app.dart';
 import 'package:trackflow/core/services/app_initializer.dart';
-import 'package:trackflow/features/projects/domain/usecases/project_usecases.dart';
-import 'package:trackflow/core/services/service_locator.dart';
-import 'package:trackflow/features/auth/presentation/bloc/auth_state.dart';
-import 'package:trackflow/features/auth/domain/usecases/auth_usecases.dart';
 import 'package:trackflow/core/dev/dev_sign_in.dart';
+import 'package:trackflow/core/services/service_locator.dart';
 
 void main({bool testMode = false, bool devSignIn = true}) async {
   debugPrint('main() called with testMode: $testMode');
@@ -22,14 +10,6 @@ void main({bool testMode = false, bool devSignIn = true}) async {
   await initializer.initialize();
   setupProjectDependencies(testMode: testMode);
   await setupAuthDependencies(initializer.prefs);
-
-  runApp(
-    MyApp(
-      prefs: initializer.prefs,
-      onboardingRepository: initializer.onboardingRepository,
-      testMode: testMode,
-    ),
-  );
 
   // After the first frame, trigger dev sign-in if needed
   if (devSignIn) {
@@ -42,66 +22,12 @@ void main({bool testMode = false, bool devSignIn = true}) async {
       }
     });
   }
-}
 
-class MyApp extends StatelessWidget {
-  final SharedPreferences prefs;
-  final SharedPrefsOnboardingRepository onboardingRepository;
-  final bool testMode;
-
-  MyApp({
-    super.key,
-    required this.prefs,
-    required this.onboardingRepository,
-    this.testMode = false,
-  }) {
-    debugPrint('MyApp constructor called, testMode: $testMode');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiRepositoryProvider(
-      providers: [RepositoryProvider.value(value: prefs)],
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<AuthBloc>(
-            create:
-                (context) =>
-                    AuthBloc(sl<AuthUseCases>())..add(AuthCheckRequested()),
-          ),
-          BlocProvider<ProjectsBloc>(
-            create: (context) => ProjectsBloc(sl<ProjectUseCases>()),
-          ),
-          BlocProvider<OnboardingBloc>(
-            create:
-                (context) =>
-                    OnboardingBloc(onboardingRepository)
-                      ..add(OnboardingCheckRequested()),
-          ),
-        ],
-        child: BlocListener<AuthBloc, AuthState>(
-          listener: (context, state) {
-            // Removed currentUserId logic as it's no longer needed in ProjectsBloc
-          },
-          child: App(testMode: testMode),
-        ),
-      ),
-    );
-  }
-}
-
-class App extends StatelessWidget {
-  final bool testMode;
-  App({super.key, this.testMode = true}) {
-    debugPrint('App constructor called, testMode: $testMode');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'TrackFlow',
-      theme: AppTheme.theme,
-      routerConfig: AppRouter.router(context, testMode: testMode),
-    );
-  }
+  runApp(
+    MyApp(
+      prefs: initializer.prefs,
+      onboardingRepository: initializer.onboardingRepository,
+      testMode: testMode,
+    ),
+  );
 }
