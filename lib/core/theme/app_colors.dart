72b45d4440db72e411c10d04a7b46802473dbc2f
Thomas Viana <thomasviana16@gmail.com>
2025-06-17 11:11:54 -0500

    Add theme stuff.

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF00B4D8);
+  static const Color secondary = Color(0xFF2EC4B6);
+  static const Color background = Color(0xFF121212);
+  static const Color surface = Color(0xFF1E1E1E);
+  static const Color error = Color(0xFFCF6679);
+}
