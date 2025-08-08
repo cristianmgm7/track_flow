9a4520c0972b6c0aecad96a8fac65217cfdf6d31
Cristian Murillo <cristianmgm7@gmail.com>
2025-08-08 18:35:49 +0200

    fix(ui/modals): revert form sheets to use rootNavigator=false by default to keep screen BLoC context and avoid hangs

--- lib/features/ui/modals/app_form_sheet.dart
+++ lib/features/ui/modals/app_form_sheet.dart
@@ -11,7 +11,7 @@ Future<T?> showAppFormSheet<T>({
   double minChildSize = 0.5,
   double maxChildSize = 0.9,
   bool isDismissible = true,
-  bool useRootNavigator = true,
+  bool useRootNavigator = false,
 }) {
   return showModalBottomSheet<T>(
     context: context,
